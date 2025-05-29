78c0aee9f75d402bdd28be15703a992ab7509452
Nerros1984 <javierhernandezav@gmail.com>
2025-05-30 00:14:30 +0200

    Update sheets.py

--- utils/sheets.py
+++ utils/sheets.py
@@ -3,17 +3,16 @@ from oauth2client.service_account import ServiceAccountCredentials
 import streamlit as st
 
 
-def registrar_en_sheet(nombre_temario, tipo_contenido, url_pdf, url_json, fecha):
+def registrar_en_sheet(nombre_oposicion, nombre_temario, tipo_contenido, enlace_pdf, enlace_json, fecha):
     creds = ServiceAccountCredentials.from_json_keyfile_dict(
         st.secrets["gcp_service_account"],
         ["https://spreadsheets.google.com/feeds", "https://www.googleapis.com/auth/drive"]
     )
     client = gspread.authorize(creds)
     sheet = client.open("EvaluaYa_base").worksheet("temarios")
-    
-    fila = [tipo_contenido, nombre_temario, url_pdf, url_json, "", fecha]
-    sheet.append_row(fila)
 
+    fila = [nombre_oposicion, nombre_temario, tipo_contenido, enlace_pdf, enlace_json, fecha]
+    sheet.append_row(fila)
 
 def obtener_oposiciones_guardadas():
     creds = ServiceAccountCredentials.from_json_keyfile_dict(
