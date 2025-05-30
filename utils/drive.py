e15e1a08f4806f4b8d06aed140b8fb6e1e40dbe0
Nerros1984 <javierhernandezav@gmail.com>
2025-05-30 17:07:55 +0200

    Update drive.py

--- utils/drive.py
+++ utils/drive.py
@@ -1,11 +1,12 @@
-import os
 import re
 import unicodedata
+import os
 from pydrive.auth import GoogleAuth
 from pydrive.drive import GoogleDrive
 from oauth2client.service_account import ServiceAccountCredentials
 import streamlit as st
 
+# IDs de las carpetas raíz en Google Drive
 CARPETA_TEMARIOS = "1x08mfjA7JhnVk9OxvXJD54MLmtdZDCJb"
 CARPETA_TEMAS_JSON = "1popTRkA-EjI8_4WqKPjkldWVpCYsJJjm"
 CARPETA_TEST_JSON = "1dNkIuLDfV_qGmrCepkFYo5IWlfFwkl7w"
@@ -13,30 +14,46 @@ CARPETA_TEST_PDF = "1dNkIuLDfV_qGmrCepkFYo5IWlfFwkl7w"
 
 def autenticar_drive():
     scope = ['https://www.googleapis.com/auth/drive']
-    credentials = ServiceAccountCredentials.from_json_keyfile_dict(st.secrets["gcp_service_account"], scope)
+    credentials = ServiceAccountCredentials.from_json_keyfile_dict(
+        st.secrets["gcp_service_account"],
+        scope
+    )
     gauth = GoogleAuth()
     gauth.credentials = credentials
     return GoogleDrive(gauth)
 
 def normalizar_nombre(nombre):
     nombre = unicodedata.normalize('NFKD', nombre).encode('ascii', 'ignore').decode('utf-8')
-    return re.sub(r'[^a-zA-Z0-9]', '_', nombre.lower())
+    nombre = re.sub(r'[^a-zA-Z0-9\\s]', '', nombre)
+    nombre = nombre.lower().strip().replace(' ', '_')
+    return nombre
 
-def obtener_o_crear_carpeta(drive, nombre_temario, carpeta_raiz_id):
-    nombre_norm = normalizar_nombre(nombre_temario)
-    query = f"title='{nombre_norm}' and mimeType='application/vnd.google-apps.folder' and '{carpeta_raiz_id}' in parents"
+def obtener_o_crear_carpeta(drive, nombre_temario, carpeta_raiz_id=None):
+    nombre_normalizado = normalizar_nombre(nombre_temario)
+    query = f"title='{nombre_normalizado}' and mimeType='application/vnd.google-apps.folder'"
+    if carpeta_raiz_id:
+        query += f" and '{carpeta_raiz_id}' in parents"
     carpetas = drive.ListFile({'q': query}).GetList()
     if carpetas:
         return carpetas[0]['id']
-    carpeta = drive.CreateFile({'title': nombre_norm, 'mimeType': 'application/vnd.google-apps.folder', 'parents': [{'id': carpeta_raiz_id}]})
+    metadata = {
+        'title': nombre_normalizado,
+        'mimeType': 'application/vnd.google-apps.folder'
+    }
+    if carpeta_raiz_id:
+        metadata['parents'] = [{'id': carpeta_raiz_id}]
+    carpeta = drive.CreateFile(metadata)
     carpeta.Upload()
     return carpeta['id']
 
-def subir_archivo_a_drive(ruta_local, nombre_temario, carpeta_raiz_id):
+def subir_archivo_a_drive(ruta_archivo, nombre_temario, carpeta_raiz_id):
     drive = autenticar_drive()
     carpeta_id = obtener_o_crear_carpeta(drive, nombre_temario, carpeta_raiz_id)
-    nombre_archivo = os.path.basename(ruta_local)
-    archivo = drive.CreateFile({'title': nombre_archivo, 'parents': [{'id': carpeta_id}]})
-    archivo.SetContentFile(ruta_local)
-    archivo.Upload()
-    return archivo['alternateLink']
+    nombre_archivo = os.path.basename(ruta_archivo)
+    archivo_drive = drive.CreateFile({
+        'title': nombre_archivo,
+        'parents': [{'id': carpeta_id}]
+    })
+    archivo_drive.SetContentFile(ruta_archivo)
+    archivo_drive.Upload()
+    return archivo_drive['alternateLink']
