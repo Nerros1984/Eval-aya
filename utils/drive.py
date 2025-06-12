0f7cbc868b199eb71796d12e590cb86560cc5a4b
Nerros1984 <javierhernandezav@gmail.com>
2025-06-12 11:13:31 +0200

    Update drive.py

--- utils/drive.py
+++ utils/drive.py
@@ -1,34 +1,59 @@
-
 import os
+import json
+from google.oauth2 import service_account
 from googleapiclient.discovery import build
 from googleapiclient.http import MediaFileUpload
-from google.oauth2 import service_account
 import streamlit as st
 
-SCOPES = ['https://www.googleapis.com/auth/drive']
-CARPETA_TEST_PDF = "1dNkIuLDfV_qGmrCepkFYo5IWlfFwkl7w"
-CARPETA_TEST_JSON = "1dNkIuLDfV_qGmrCepkFYo5IWlfFwkl7w"
+# Credenciales desde streamlit secrets
+credentials_dict = st.secrets["gcp_service_account"]
+credentials = service_account.Credentials.from_service_account_info(credentials_dict)
 
-def autenticar_drive():
-    credentials = service_account.Credentials.from_service_account_info(
-        st.secrets["gcp_service_account"], scopes=SCOPES)
-    return build("drive", "v3", credentials=credentials)
+drive_service = build("drive", "v3", credentials=credentials)
 
-def subir_archivo_a_drive(ruta_archivo, nombre_oposicion, carpeta_id):
-    servicio = autenticar_drive()
+# Carpetas por defecto (puedes ajustar estos IDs si quieres usar constantes fijas)
+CARPETA_TEST_JSON = "test_json"
+CARPETA_TEST_PDF = "test_pdf"
+
+# Función auxiliar para buscar o crear carpetas
+
+def obtener_o_crear_carpeta(nombre_carpeta, carpeta_padre_id=None):
+    query = f"name='{nombre_carpeta}' and mimeType='application/vnd.google-apps.folder' and trashed=false"
+    if carpeta_padre_id:
+        query += f" and '{carpeta_padre_id}' in parents"
+
+    respuesta = drive_service.files().list(q=query, spaces='drive').execute()
+    archivos = respuesta.get('files', [])
+
+    if archivos:
+        return archivos[0]['id']
+
+    metadata = {
+        'name': nombre_carpeta,
+        'mimeType': 'application/vnd.google-apps.folder'
+    }
+    if carpeta_padre_id:
+        metadata['parents'] = [carpeta_padre_id]
+
+    archivo = drive_service.files().create(body=metadata, fields='id').execute()
+    return archivo['id']
+
+
+def subir_archivo_a_drive(ruta_archivo, subcarpeta, carpeta_padre_id=None):
     nombre_archivo = os.path.basename(ruta_archivo)
+    carpeta_id = obtener_o_crear_carpeta(subcarpeta, carpeta_padre_id)
 
     archivo_metadata = {
-        "name": nombre_archivo,
-        "parents": [carpeta_id]
+        'name': nombre_archivo,
+        'parents': [carpeta_id]
     }
 
     media = MediaFileUpload(ruta_archivo, resumable=True)
-    archivo = servicio.files().create(
+
+    archivo = drive_service.files().create(
         body=archivo_metadata,
         media_body=media,
-        fields="id"
+        fields='id, webViewLink'
     ).execute()
 
-    enlace = f"https://drive.google.com/file/d/{archivo.get('id')}/view?usp=sharing"
-    return enlace
+    return archivo.get('webViewLink')
