22ab953f851d3fef1c38b39e63993774024a0e4b
Nerros1984 <javierhernandezav@gmail.com>
2025-06-12 09:58:10 +0200

    Update test_generator.py

--- core/test_generator.py
+++ core/test_generator.py
@@ -3,8 +3,8 @@
 import uuid
 from datetime import datetime
 
-from .estructura import obtener_estructura_test
-from .temas import dividir_temas_en_dict
+from core.estructura import obtener_estructura_test
+from core.temas import dividir_temas_en_dict
 from utils.gpt import generar_preguntas_gpt
 
 
