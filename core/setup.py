fd0da94481b852f4cd7a19366cd10992d97ad9f1
Nerros1984 <javierhernandezav@gmail.com>
2025-06-12 10:24:40 +0200

    Delete core/setup.py

--- core/setup.py
+++
@@ -1,8 +0,0 @@
-# setup.py
-from setuptools import setup, find_packages
-
-setup(
-    name="evaluaya",
-    version="0.1",
-    packages=find_packages(include=["core", "core.*", "utils", "utils.*", "ui", "ui.*"]),
-)
